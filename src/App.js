5e0ba71681d979346d632cbb45c8d4850fa028cf
satwikmvv <satwikmvv@gmail.com>
2018-07-14 11:07:49 -0400

    -handled errors -search by pokename to view their id and image

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      pokenumber: '',
-      pokename:''
+      pokenumber: null,
+      pokename:'',
+      pokeimg:'',
+      err:''
     }
     this.handleChange=this.handleChange.bind(this);
     this.handleSubmit=this.handleSubmit.bind(this);
   }
-  componentDidMount() {
-    fetch(`http://pokeapi.co/api/v2/pokemon/?limit=200&offset=0`)
-      .then(x=>x.json())
-      .then(x=>console.log(x))
-  }
+  
   handleChange=(e)=>{
     this.setState({
       [e.target.name]: e.target.value
@@ -24,23 +22,39 @@ class App extends Component {
   }
   handleSubmit=(e)=>{
     e.preventDefault();
-    fetch(`http://pokeapi.co/api/v2/pokemon/${this.state.pokenumber}`)
+    this.setState({
+      pokename:'',
+      pokeimg:'',
+      pokenumber:null
+    })
+    fetch(`http://pokeapi.co/api/v2/pokemon/${this.state.pokename}`)
       .then(x=>x.json())
       .then(x=>{
         console.log(x);
         this.setState({
-          pokename: x.name
+          pokenumber: x.id,
+          pokeimg:x.sprites.front_default
         })
       })
+      .catch((error)=>this.setState({
+        err:'Pokemon Not Found'
+      }))
+    
   }
   render() {
     return (
       <div className="App">
         <form onSubmit={this.handleSubmit} >
-          Pokemon Number:<input onChange={this.handleChange} name='pokenumber' value={this.state.pokenumber}/>
+          Pokemon:<input onChange={this.handleChange} name='pokename' value={this.state.pokename}/>
           <button>Submit</button>
         </form>
-        {(this.state.pokename)?this.state.pokename :'no'}
+        {(this.state.pokenumber)?
+          <div>
+            <h1>Pokemon Index number:{this.state.pokenumber}</h1>
+            <img src={this.state.pokeimg} alt=''/>
+          </div>
+          :<h1>{this.state.err}</h1>
+        }
       </div>
     );
   }
